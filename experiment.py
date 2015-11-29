c48fae5848ce3fbfaa6f7ebd0c0be3b17cd1486a
dongzhangfan <dong.zhangfan@gmail.com>
2015-11-29 15:05:39 -0800

    Fix executable path error.

--- experiment.py
+++ experiment.py
@@ -11,8 +11,9 @@ with open('result.cvs', 'w') as file:
     file.write("move, score, max rank\n")
     for i in range(EXPERIMENT_TIMES):
         p = subprocess.Popen(
-            './threes', stderr=subprocess.PIPE, stdout=DEVNULL)
+            './bin/threes', stderr=subprocess.PIPE, stdout=DEVNULL)
         for line in p.stderr.readlines():
             file.write(line)
+        print("Test #" + i + " Finished.")
 
 retval = p.wait()
