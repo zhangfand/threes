3a7ac5ec104a79cdee4cf53531c8d3740b4d8b3f
Chin-I <b92703083@gmail.com>
2015-11-28 22:34:29 -0800

    static float SCORE_MERGES_WEIGHT = 10.0f;  static float SCORE_12_MERGES_WEIGHT = 10.0f;

--- threes.cpp
+++ threes.cpp
@@ -63,12 +63,12 @@ static float score_table[65536];
 // Heuristic scoring settings
 static float SCORE_LOST_PENALTY = 200000.0f;
 static float SCORE_MONOTONICITY_POWER = 4.0f;
-static float SCORE_MONOTONICITY_WEIGHT = 47.0f;
+static float SCORE_MONOTONICITY_WEIGHT = 0.0f;
 static float SCORE_SUM_POWER = 3.5f;
-static float SCORE_SUM_WEIGHT = 11.0f;
-static float SCORE_MERGES_WEIGHT = 700.0f;
-static float SCORE_12_MERGES_WEIGHT = 0.0f;
-static float SCORE_EMPTY_WEIGHT = 270.0f;
+static float SCORE_SUM_WEIGHT = 0.0f;
+static float SCORE_MERGES_WEIGHT = 10.0f;
+static float SCORE_12_MERGES_WEIGHT = 10.0f;
+static float SCORE_EMPTY_WEIGHT = 0.0f;
 
 void set_heurweights(float *f, int flen) {
     if(flen != 7) {
